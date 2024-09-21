2fbee00ff1689e2ff6252a73cda604b7ba35fec1
Kenneth Stallworth <kennethstallworth@hotmail.com>
2024-09-21 08:30:23 -0400

    modified home page

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,22 @@ import './index.css'
 import {BrowserRouter , Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import JetsHome from "./pages/JetsHome"												
 function App(){
 	return(
       	<BrowserRouter>
+	<header>
+		<Link to="/">#JETSETLIFE</Link> 
 		<nav>
 		<Link to ="/">Home</Link>
 		<Link to="/about">About</Link>
+		<Link to="/jets">Jets Home</Link>
 		</nav>
+	</header>
     	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path ="/about" element={<About />} />
+		<Route path ="/jets" element={<JetsHome />} />
     	</Routes>
   	</BrowserRouter>
 	)
