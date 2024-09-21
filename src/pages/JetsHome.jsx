6a9ea91d3756f27a2bc1ccc5f3c198ec643ee308
Kenneth Stallworth <kennethstallworth@hotmail.com>
2024-09-20 23:40:12 -0400

    Jets Home page

---
+++ src/pages/JetsHome.jsx
@@ -0,0 +1,57 @@
+//JUST the start of the jets information pages 
+
+
+export default function JetsHome(){
+
+	return(
+		<>
+			<h1>Jet Types and their typical ranges</h1>
+		
+			<div>
+				<section>
+					<h2>Turboprop</h2>
+						<p>When most people think “private plane” and executive jet comes to mind. However, quite a number of “private jet” charters, are not flown on jets at all. If you think propeller planes are a thing of the past, think again! Quite a number of private business flights are made on modern, turboprop aircraft. These planes are ideal for shorter flights, of to get private jet charter clients into and out of airports that even the smallest jets cannot get to. When a turboprop will do, you can save significantly!</p>
+				</section>
+			</div>
+
+			<div>
+				<section>
+					<h2>Light Jets</h2>
+					<p>Light jets are the typical “executive jet.”  Light jets are small, and speedy, designed for the business traveler who needs to do regular “city to city” hops such as New York to DC, or Boston to Atlanta. They have a range of about 700 – 2000 miles. The Hawker 400 XP boasts great performance and one of the most spacious cabins in the category.</p>
+				</section>
+			</div>
+			
+			<div>
+				<section>
+					<h2>Mid-Size Jets</h2>
+					<p>Mid-size jets are designed for smaller groups, accommodating usually about 3 – 5 passengers in comfort. They are somewhat slower than mid-size jets, with a top speed of about 530mph. Their range is about 1300 – 1500 miles, making them ideal for our most popular route – New York to Miami. The most prominent jet in this category is the Hawker 800XP.</p>
+				</section>
+			</div>
+			
+			<div>
+				<section>
+				<h2>Super Mid-Size Jets</h2>
+				<p>This is a very popular class of private jets.  Super-mids are ideal for intermediate to large groups of passengers. They have been designed to be a very fast and cost-effective way to fly between routes in the 3500 – 4000-mile range. This makes them ideal for flights such as from Boston to LA, or Seattle to Tampa. Super Mid-Size jets can accommodate up to nine passengers and are very fast. In fact, the most popular private jet in this category, the Cessna Citation X, is known as the fastest airplane in the world for civilian use.</p>
+				</section>
+			</div>
+			
+			<div>
+				<section>
+					<h2>Heavy Jets</h2>
+					<p>Heavy jets are designed for flyers that tend to fly with large groups of passengers, those who need to fly internationally, or coast-to-coast nonstop, or just desire the ultimate in comfort  and luxury.  Heavy jets have the longest range of  all classes of private jet. They can easily carry up to 12 passengers in complete comfort, can achieve speeds of nearly 600mph, and have more than a 5000-mile range. A heavy jet can fly non-stop New York to Paris, or Los Angeles to Moscow. The most well-known private jets in this category  are the Gulfstream G450 and Gulfstream GIV.</p>
+				</section>
+			</div>
+			<div>
+				<section>
+					<h2>Ultra-Long Range</h2>
+						<p>Ultra-long range jets are the epitome of style and performance of private jets. Ultra-long range heavy jets are superbly appointed with loads of passenger and baggage space, stand-up cabins and lie-flat beds. Enclosed bathrooms are a standard feature.</p>
+
+					
+				</section>
+			</div>
+			
+
+		</>
+	)
+
+}
